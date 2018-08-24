e362bdb06b1c76472a3283f3f11c35da25f619e9
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-08-24 10:25:16 +0900

    Add uniqueness constraint of user email as database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_23_054748) do
+ActiveRecord::Schema.define(version: 2018_08_24_012110) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2018_08_23_054748) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
