ad7adc32e7c7c8b40c0cdfa42f9051db4265d49f
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-08-27 13:34:58 +0900

    Define before action for redirect before login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
   include SessionsHelper
   before_action :current_user, only: :index
   before_action :redirect_if_logged_in, only: [:new, :create]
+  before_action :redirect_unless_logged_in, only: [:index]
 
   def index
-    redirect_to login_path, notice: "ログインしてください。" and return unless logged_in?
     @users = User.all
   end
 
@@ -26,5 +26,9 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :email, :password)
+    end
+
+    def redirect_unless_logged_in
+      redirect_to login_path, notice: "ログインしてください。" and return unless logged_in?
     end
 end
